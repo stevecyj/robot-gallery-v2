b8278cd077da6accaebb5c7ff232aea4ad5d7629
Steve <steve.tsao@hlw.lan>
2022-10-21 16:44:18 +0800

    setState

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import logo from './assets/images/logo.svg';
-import robots from './mockdata/robots.json';
+// import robots from './mockdata/robots.json';
 import Robot from './components/Robot';
 import styles from './App.module.css';
 import ShoppingCart from './components/ShoppingCart';
-import { isConstructorDeclaration } from 'typescript';
 
 interface Props {}
 
 interface State {
   robotGallery: any[];
+  count: number;
 }
 class App extends React.Component<Props, State> {
   constructor(props) {
     super(props);
     this.state = {
-      robotGallery: []
+      robotGallery: [],
+      count: 0
     };
   }
 
@@ -32,6 +33,29 @@ class App extends React.Component<Props, State> {
           <img src={logo} className={styles.appLogo} alt="" />
           <h1>羅伯特機器人炫酷炸翻天online購物平台名字要長</h1>
         </div>
+        <button
+          onClick={() => {
+            this.setState(
+              (preState, preProps) => {
+                return { count: preState.count + 1 };
+              },
+              () => {
+                console.log('count: ', this.state.count);
+              }
+            );
+            this.setState(
+              (preState, preProps) => {
+                return { count: preState.count + 1 };
+              },
+              () => {
+                console.log('count: ', this.state.count);
+              }
+            );
+          }}
+        >
+          Click
+        </button>
+        <span>count: {this.state.count}</span>
         <ShoppingCart />
         <div className={styles.robotList}>
           {this.state.robotGallery.map((r) => (
